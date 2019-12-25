e293fc68bdc1a5f1cddc430884dca6ff2ca92d03
Xiar <54110012+Xiar-fatah@users.noreply.github.com>
2019-12-25 22:26:21 +0100

    Update FJC_NON_SAW.py

--- FJC_NON_SAW.py
+++ FJC_NON_SAW.py
@@ -1,24 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
-#%matplotlib qt, for interactive
-#Three dimensional random walk, freely jointed chain, SAW
 
-#Number of steps
-num_of_steps = 1000
+#Self avoiding walk, freely jointed chain
 
-#Store x,y,z values, note the random walk begins in origo
-x = [0] 
-y = [0] 
-z = [0]
+def rand_walk(x,y,z,n):
+    for i in range(0,n):
+            
+        vec = rand_vec()
 
+        x.append(x[i]+vec[0])
+        y.append(y[i]+vec[1])
+        z.append(z[i]+vec[2])
+    
+    return x,y,z
+
+    
 #A function that generates a random vector and normalizes it
+#Intuitive, a new direction is created
 def rand_vec():
     #vec[0] = x, vec[1] = y ....
     vec = []
@@ -26,20 +25,37 @@ def rand_vec():
         vec.append(np.random.normal(0,1,1))
     
     norm = np.sqrt(pow(vec[0],2) + pow(vec[1],2) + pow(vec[2],2))
-    np.multiply(vec,norm**(-1))
+    vec = np.multiply(vec,norm**(-1))
     return vec
 
-#Random walk
-for i in range(0,num_of_steps):
+if __name__ == "__main__":
+    #Number of steps
+    num_of_steps = 1000
     
-    vec = rand_vec()
+    #Store x,y,z values, note the random walk begins in origo
+    x = [0]
+    y = [0] 
+    z = [0] 
     
-    #The last position is also appended
-    x.append(x[i] + vec[0])
-    y.append(y[i] + vec[1])
-    z.append(z[i] + vec[2])
-
-#Three dimensional plot mabye try to plot in matlab instead? 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot3D(x, y, z, 'red')
+    res = rand_walk(x,y,z, num_of_steps)
+    print(res)
+    #Checks the magntidue of each vector
+    store_coord = []
+    for i in range(0,num_of_steps+1):
+        store_coord.append([x[i],y[i],z[i]])
+        print(np.linalg.norm(store_coord))
+        store_coord = []
+        
+    
+    #Three dimensional plot mabye try to plot in matlab instead? 
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.plot(res[0], res[1], res[2], linestyle = '-')
+    ax2 = fig.gca(projection='3d')
+    ax2.scatter([0], [0], [0], c = 'r', marker = 'x')
+    plt.legend(['Polymer', 'Origin'])
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('z')
+    
+    plt.show()
